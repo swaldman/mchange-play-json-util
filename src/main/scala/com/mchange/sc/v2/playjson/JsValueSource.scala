06ec963ab7a60dfe548f9728ab831311d0e52ecc
Steve Waldman <swaldman@mchange.com>
2017-10-31 15:38:52 -0700

    Define typeclass for java.nio.ByteBuffer as JsValueSource

--- src/main/scala/com/mchange/sc/v2/playjson/JsValueSource.scala
+++ src/main/scala/com/mchange/sc/v2/playjson/JsValueSource.scala
@@ -3,6 +3,8 @@ package com.mchange.sc.v2.playjson
 import java.io.InputStream
 import play.api.libs.json._
 
+import java.nio.ByteBuffer
+
 object JsValueSource {
   implicit final object JsValueIsSource extends JsValueSource[JsValue] {
     def toJsValue( jsv : JsValue ) : JsValue = jsv
@@ -18,6 +20,26 @@ object JsValueSource {
   }
   implicit final object InputStreamIsSource extends JsValueSource[InputStream] {
     def toJsValue( is : InputStream ) : JsValue = Json.parse( is )
+  }
+  implicit final object ByteBufferIsSource extends JsValueSource[ByteBuffer] {
+    def toJsValue( bb : ByteBuffer ) : JsValue = Json.parse( newArray( bb ) )
+  }
+
+  // translated from mchange-commons-java com.mchange.v3.nio.ByteBufferUtils.newArray(...) to avoid
+  // a dependency on that library for just this
+  //
+  // if this library eventually comes to depend on mchange-commons-java (or mchange-commons-scala,
+  // which depends on mchange-commons-java), then get rid of this and just use that function
+
+  private def newArray( bb : ByteBuffer ) : Array[Byte] = {
+    if ( bb.hasArray() ) {
+      bb.array().clone().asInstanceOf[Array[Byte]];
+    }
+    else {
+      val out = Array.ofDim[Byte](bb.remaining())
+      bb.get(out)
+      out;
+    }
   }
 }
 trait JsValueSource[T] {
