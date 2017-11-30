f19c94c48d3dc552a92558eb807b54851787dde7
Steve Waldman <swaldman@mchange.com>
2017-11-30 14:05:38 -0800

    Fix bad construction of ImmutableArraySeq.Byte.

--- src/main/scala/com/mchange/sc/v2/playjson/BufferedJsValue.scala
+++ src/main/scala/com/mchange/sc/v2/playjson/BufferedJsValue.scala
@@ -13,11 +13,8 @@ import java.nio.ByteBuffer
 
 import com.mchange.v3.nio.ByteBufferUtils
 
-
-
-
 object BufferedJsValue {
-  def apply( arr : Array[Byte] ) : BufferedJsValue = this.apply( ImmutableArraySeq.apply[Byte](arr) ) // this could be from anywhere, better copy it
+  def apply( arr : Array[Byte] ) : BufferedJsValue = this.apply( ImmutableArraySeq.Byte(arr) ) // this could be from anywhere, better copy it
   def apply( jsv : JsValue     ) : BufferedJsValue = this.apply( Json.stringify( jsv ) )
   def apply( is  : InputStream ) : BufferedJsValue = this.apply( ImmutableArraySeq.Byte.createNoCopy( InputStreamUtils.getBytes( is ) ) )
   def apply( str : String      ) : BufferedJsValue = this.apply( ImmutableArraySeq.Byte.createNoCopy( str.getBytes( Codec.UTF8.charSet ) ) )
